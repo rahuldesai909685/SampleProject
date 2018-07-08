293151757f5e6a971bbe0d20f793f16579995e01
rahuldesai9011 <40596118+rahuldesai9011@users.noreply.github.com>
2018-07-08 19:08:25 +0530

    SampleProject testing

---
+++ src/com/jenkins/samples/JenkinDemo.java
@@ -0,0 +1,22 @@
+package com.jenkins.samples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinDemo {
+
+	static WebDriver driver;
+	
+	@Test
+	public void testSample() {
+		System.out.println("welcome to jenkins world");
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		System.out.println("1");
+		driver.manage().window().maximize();
+		System.out.println("2");
+		
+			
+	}
+}
